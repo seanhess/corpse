16f9da47125d55bfe88cb2807cd3995f020c52d4
Jason Hills <jasonahills@gmail.com>
2014-01-14 11:44:26 -0700

    beginnning newBody function

--- public/js/cards.js
+++ public/js/cards.js
@@ -4,10 +4,38 @@
 
 // }
 
-function generateConnectingPoints(edgeLength, numPoints){
+function newBody(){
+	//generate
+	var edges = {
+		neck : newEdgeConnectors(1, 2),
+		leftShoulder : newEdgeConnectors(1, 2),
+		rightShoulder : newEdgeConnectors(1, 2),
+		waist : newEdgeConnectors(1, 2),
+		leftThigh : newEdgeConnectors(1, 2),
+		rightThigh : newEdgeConnectors(1, 2),
+		leftAnkle : newEdgeConnectors(1, 2),
+		rightAnkle: newEdgeConnectors(1, 2)
+	}
+	console.log("edges", edges)
+
+
+	//newHead
+	//newTorso
+	//newHips
+
+
+
+}
+
+function newEdgeConnectors(edgeLength, numPoints){
 	var points = [];
 	for (var i = 0; i < numPoints; i++){
 		points.push(Math.random()*edgeLength)
 	}
 	return points.sort()
+}
+
+function placeConnectors(side, connectorsArray){
+
+
 }
