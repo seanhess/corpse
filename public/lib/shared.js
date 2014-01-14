2cf814ec2975dce16e07359450a39e500c2a0baa
Sean Clark Hess <seanhess@gmail.com>
2014-01-14 11:22:27 -0700

    grunt install works

---
+++ public/lib/shared.js
@@ -0,0 +1,32 @@
+
+// just an object
+function SharedObject() {
+
+}
+
+function SharedCollection() {
+
+}
+
+
+
+/*
+DATA MODEL
+game
+    pieces: {
+        head:  Piece,
+        torso: Piece,
+        hips: Piece,
+        rightArm: Piece,
+        leftArm: Piece,
+        leftLeg: Piece,
+        rightLeg: Piece,
+        leftFoot: Piece,
+        rightFoot: Piece,
+    }
+
+piece: {
+    data: ??? 
+}
+
+*/
